eae6601084f7f6a8826d791afbb4fb8cfb2b1b20
Karla <arq.kparada@gmail.com>
2016-03-17 21:42:03 -0400

    add player

--- js/app.js
+++ js/app.js
@@ -24,12 +24,25 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 10;
+    this.y = 10;
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function() {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [ new Enemy(2) ];
+var player =  new Player();
 
 
 // This listens for key presses and sends the keys to your
